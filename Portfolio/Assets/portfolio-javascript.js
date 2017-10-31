369ce7d377243eb821d5303663f641368cd63f68
MrJAmos <jude.amos@qa.com>
2017-10-31 07:15:12 +0000

    networking done.

--- Portfolio/Assets/portfolio-javascript.js
+++ Portfolio/Assets/portfolio-javascript.js
@@ -1,5 +1,28 @@
 
 
+function func() {
+    'use strict';
+    let menuIcon = document.getElementById("menuIcon");
+    let aTag = document.getElementById("portfoliotext");
+    let node = document.createTextNode("Portfolio");
+    aTag.insertBefore(node,menuIcon);
+
+    let mq = window.matchMedia("(min-width: 736px)");
+    
+    if (!mq.matches) {
+        aTag.removeAttribute('href');
+    }
+    else {
+        let x = document.getElementsByTagName("TITLE")[0].textContent;
+        if(x === "Contact Page"){
+            aTag.setAttribute('href', '../../index.html');                    
+        }
+        else if(x === "Jude Amos"){
+            aTag.setAttribute('href', 'index.html');                                
+        }
+    }
+}
+
 function dropMenu() {
     'use strict';
     let x = document.getElementById('display-menu');
@@ -18,4 +41,12 @@ function dropMenu() {
         }
     }
 }
+
+window.onload = function() {
+    func();
+}
+
+
+
+
 
